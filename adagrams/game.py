67d616b6538025b8f33382b6b51a1f0f5ec535c4
rayne <pothong.r@gmail.com>
2026-09-10 16:24:48 -0700

    added score_word function

--- adagrams/game.py
+++ adagrams/game.py
@@ -59,7 +59,45 @@ def uses_available_letters(word, letter_bank):
     return True
     
 def score_word(word):
-    pass
+    LETTER_VALUES = {
+    "A": 1,
+    "B": 3,
+    "C": 3,
+    "D": 2,
+    "E": 1,
+    "F": 4,
+    "G": 2,
+    "H": 4,
+    "I": 1,
+    "J": 8,
+    "K": 5,
+    "L": 1,
+    "M": 3,
+    "N": 1,
+    "O": 1,
+    "P": 3,
+    "Q": 10,
+    "R": 1,
+    "S": 1,
+    "T": 1,
+    "U": 1,
+    "V": 4,
+    "W": 4,
+    "X": 8,
+    "Y": 4,
+    "Z": 10
+}
+    score = 0
+    # look up letter value in dictionary
+    for letter in word:
+        letter = letter.upper()
+        # add the correct letter value to score
+        score += LETTER_VALUES[letter]
+    # if word length is between 7 and 10 add 8 to score
+    if 7 <= len(word) <= 10:
+        score += 8
+
+    return score
 
 def get_highest_word_score(word_list):
     pass
